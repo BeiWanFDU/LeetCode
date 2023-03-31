567f1f86e4e10fa8f7661dafb93bcde6026592ed
kang <kang@gamil.com>
2023-03-31 13:02:02 +0800

    commit 2023/3/31 13:02

--- 01两数之和.cpp
+++ 01两数之和.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout<<"hello"<<endl;
-    cout<<"world"<<endl;
-    cout<<"world"<<endl;
-    cout<<"world"<<endl;
-    cout<<"world"<<endl;
+    
 }
